0feeccbd784b44654283d03e5438c3a006384a25
denver <denver@semiotic.ai>
2024-12-19 12:05:50 -0600

    test: add test_prompt_entity_comparison

--- graphdoc/tests/test_entity_comparison_tests.py
+++ graphdoc/tests/test_entity_comparison_tests.py
@@ -9,6 +9,14 @@ class TestEntityComparison:
     def test_with_fire(self):
         assert True 
 
+    @pytest.mark.skipif("not config.getoption('--fire')")
+    def test_prompt_entity_comparison(self, gd, entity_comparison_assets):
+        response = gd.prompt_entity_comparison(entity_comparison_assets["gold_entity_comparison"], entity_comparison_assets["gold_entity_comparison"])
+        response = gd.language_model.parse_response(response)
+        
+        assert response["correctness"] in [1, 2, 3, 4]
+        assert isinstance(response["reasoning"], str)
+
     # def test_graphdoc_fixture(self, gd: GraphDoc):
     #     assert gd.language_model != None
 
