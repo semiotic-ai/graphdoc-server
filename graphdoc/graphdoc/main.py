ac4ecee1c3a8c2049369a46e262ba3fa02e7f738
denver <denver@semiotic.ai>
2024-12-19 11:25:51 -0600

    feat: add prompt revision functionality

--- graphdoc/graphdoc/main.py
+++ graphdoc/graphdoc/main.py
@@ -36,6 +36,12 @@ class LanguageModel:
             return response
         except Exception as e:
             logging.error(f"Prompt failed: {e}")
+    
+    def parse_response(self, response): 
+        response = response.choices[0].message.content
+        response = response.strip('```json').strip('```').strip()
+        response = json.loads(response)
+        return response
 
 class GraphDoc:
     def __init__(
@@ -58,13 +64,57 @@ class GraphDoc:
         # load in our jinja based prompt templates
         self.entity_comparison_prompt_env = Environment(loader=FileSystemLoader(prompt_templates_dir))
         self.entity_comparison_prompt_template = self.entity_comparison_prompt_env.get_template("entity_comparison_prompt.txt") # TODO: we can move this out to a config file
+        self.entity_comparison_prompt_revision_template = self.entity_comparison_prompt_env.get_template("entity_comparison_revision.txt") # TODO: we can move this out to a config file
+        
         logging.info(f"Initialized GraphDoc")
 
-    def generate_entity_comparison_prompt(self, entity1, entity2):
-        return self.entity_comparison_prompt_template.render(entity1=entity1, entity2=entity2)
+    def instantiate_entity_comparison_prompt(self, entity_gold, entity_pred):
+        return self.entity_comparison_prompt_template.render({"entity_gold": {entity_gold}, "entity_pred": {entity_pred}})
     
-    def prompt_entity_comparison(self, entity1, entity2):
-        prompt = self.generate_entity_comparison_prompt(entity1, entity2)
+    def format_entity_comparison_revision_prompt(self, response): 
+        response = self.language_model.parse_response(response)
+        revised_prompt = response["modified_prompt"]
+
+        # Replace the placeholder with Jinja syntax
+        revised_prompt = revised_prompt.replace(r"{entity_pred}", r"{{ entity_pred }}")
+        revised_prompt = revised_prompt.replace(r"{entity_gold}", r"{{ entity_gold }}")
+        return revised_prompt
+    
+    # TODO: we could refactor this to either be more generic or take a more specific configuration
+    def instantiate_entity_comparison_revision_prompt( 
+            self, 
+            original_prompt, 
+            four_comparison,
+            three_comparison,
+            two_comparison,
+            one_comparison,
+    ): 
+        """
+        _comparison = {
+            "reasoning": "the reasoning behind the score of the comparison between the gold and predicted entity",
+            "correctness": "a score from 1-4, where 4 is the best and 1 is the worst",
+        }
+        """
+        return self.entity_comparison_prompt_revision_template.render(
+            {
+                "original_prompt": {original_prompt}, 
+
+                "four_result_correct": {"True" if four_comparison["correctness"] == 4 else "False"},
+                "four_result": {four_comparison["reasoning"]},
+                
+                "three_result_correct": {"True" if three_comparison["correctness"] == 3 else "False"},
+                "three_result": {three_comparison["reasoning"]},
+                
+                "two_result_correct": {"True" if two_comparison["correctness"] == 2 else "False"},
+                "two_result": {two_comparison["reasoning"]},
+                
+                "one_result_correct": {"True" if one_comparison["correctness"] == 1 else "False"},
+                "one_result": {one_comparison["reasoning"]},
+            }
+        )
+    
+    def prompt_entity_comparison(self, entity_gold, entity_pred):
+        prompt = self.instantiate_entity_comparison_prompt(entity_gold, entity_pred)
         response = self.language_model.prompt(prompt)
-        extracted_response = response.choices[0].message.content
+        extracted_response = self.language_model.parse_response(response)
         return extracted_response
