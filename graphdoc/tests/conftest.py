75430364da9c3d36a8505554c245fdfc69b3aa07
denver <denver@semiotic.ai>
2024-12-18 19:15:25 -0600

    test: add test_entity_comparison_assets_fixture

--- graphdoc/tests/conftest.py
+++ graphdoc/tests/conftest.py
@@ -1,6 +1,8 @@
 # internal packages
 import os
-from typing import List
+import json
+from typing import Dict
+from pathlib import Path
 
 # external packages
 from pytest import fixture
@@ -21,3 +23,27 @@ def gd(lm: LanguageModel) -> GraphDoc:
     return GraphDoc(
         language_model = lm,
     )
+
+@fixture
+def entity_comparison_assets() -> Dict: 
+    # Set the absolute or relative path for the assets directory
+    assets_dir = Path(__file__).parent/ 'assets/'
+    assets_dir = Path(assets_dir)
+    if not assets_dir.exists():
+        raise FileNotFoundError(f"assets directory not found at: {assets_dir}")
+    
+    with open(Path(assets_dir / 'entity_comparison_assets.json'), 'r') as f:
+        entity_comparison_assets = json.load(f)
+
+    gold_entity_comparison = "".join(entity_comparison_assets["gold"]["prompt"])
+    four_entity_comparison = "".join(entity_comparison_assets["four"]["prompt"])
+    three_entity_comparison = "".join(entity_comparison_assets["three"]["prompt"])
+    two_entity_comparison = "".join(entity_comparison_assets["two"]["prompt"])
+    one_entity_comparison = "".join(entity_comparison_assets["one"]["prompt"])
+    return {
+        "gold_entity_comparison": gold_entity_comparison,
+        "four_entity_comparison": four_entity_comparison,
+        "three_entity_comparison": three_entity_comparison,
+        "two_entity_comparison": two_entity_comparison,
+        "one_entity_comparison": one_entity_comparison,
+    }
--- graphdoc/tests/test_graphdoc_build.py
+++ graphdoc/tests/test_graphdoc_build.py
@@ -13,3 +13,10 @@ class TestGraphDocBuild:
 
     def test_graphdoc_fixture(self, gd: GraphDoc):
         assert gd.language_model != None
+
+    def test_entity_comparison_assets_fixture(self, entity_comparison_assets):
+        assert entity_comparison_assets["gold_entity_comparison"] != None
+        assert entity_comparison_assets["four_entity_comparison"] != None
+        assert entity_comparison_assets["three_entity_comparison"] != None
+        assert entity_comparison_assets["two_entity_comparison"] != None
+        assert entity_comparison_assets["one_entity_comparison"] != None
