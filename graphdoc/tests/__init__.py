1439a7227861d8ab42d8d9e2fe23576d62ca5604
denver <denver@semiotic.ai>
2024-12-18 18:38:47 -0600

    test: add test_language_model initialization

--- graphdoc/tests/__init__.py
+++ graphdoc/tests/__init__.py
@@ -1,1 +1,2 @@
 from .test_entity_comparison_tests import *
+from .test_graphdoc_build import *
--- graphdoc/tests/test_graphdoc_build.py
+++ graphdoc/tests/test_graphdoc_build.py
@@ -1,6 +1,16 @@
+# system packages
+
+# internal packages
+from graphdoc import GraphDoc, LanguageModel
+
+# external packages
 import pytest
 
 class TestGraphDocBuild:
     
+    def test_language_model(self):
+        language_model = LanguageModel(api_key="test")
+        assert language_model.api_key == "test"
+
     def test_run(self):
         assert 1 == 1
