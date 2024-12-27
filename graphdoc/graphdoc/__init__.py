3ae1bf9c693f160e526e1f55149a16e00ddb2544
denver <denver@semiotic.ai>
2024-12-27 15:43:49 -0600

    feat(parser): add parser class for handling graphql and tokenization

--- graphdoc/graphdoc/__init__.py
+++ graphdoc/graphdoc/__init__.py
@@ -1,2 +1,3 @@
 from .prompt import *
 from .executor import *
+from .parser import *   
---
+++ graphdoc/graphdoc/parser.py
@@ -0,0 +1,33 @@
+# system packages 
+import json
+import logging
+from pathlib import Path
+from abc import ABC, abstractmethod
+from typing import Optional
+
+# internal packages 
+
+# external packages
+from tokencost import count_string_tokens
+from graphql import build_schema, parse, build_ast_schema, validate_schema, print_ast
+
+
+from openai import OpenAI
+from jinja2 import Environment, FileSystemLoader
+
+# configure logging
+logging.basicConfig(level=logging.INFO)
+
+class Parser: 
+    def __init__(self, schema_directory_path: Optional[str] = None): 
+        if schema_directory_path:
+            schema_directory_path = Path(schema_directory_path).resolve()
+            if not schema_directory_path.is_dir():
+                raise ValueError(f"The provided schema directory path '{schema_directory_path}' is not a valid directory.")
+        self.schema_directory_path = schema_directory_path
+
+    def parse(self, schema_file: str, schema_directory_path: Optional[str] = None):
+        schema_path = Path(schema_directory_path) / schema_file
+        schema = schema_path.read_text()
+        schema_ast = parse(schema)
+        return schema
