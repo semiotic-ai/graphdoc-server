3823507b4bd4fa08bf174710bc65bdba7ada57eb
denver <denver@semiotic.ai>
2024-12-27 16:22:59 -0600

    test: add token checks to TestParser

--- graphdoc/tests/test_parser.py
+++ graphdoc/tests/test_parser.py
@@ -1,4 +1,5 @@
 # system packages
+import logging
 from datetime import datetime
 from typing import Dict, Any
 
@@ -9,10 +10,27 @@ from graphdoc import Parser
 # external packages
 import pytest
 
+logging.basicConfig(level=logging.INFO)
+
 class TestParser: 
     def test_parse_schema(self, par: Parser): 
         schema_file = "opensea_original_schema.graphql"
         schema = par.parse_schema(schema_file)
         assert schema != None
         assert len(schema.definitions) == 9
-            
+    
+    def test_check_schema_token_count(self, par: Parser): 
+        schema_file = "opensea_original_schema.graphql"
+        schema = par.parse_schema(schema_file)
+        token_count = par.check_schema_token_count(schema)
+        logging.info(f"Token count: {token_count}")
+        assert token_count == 1774
+
+    def test_check_model_data(self, par: Parser): 
+        assert par.model_data != None
+        assert "gpt-4o" in par.model_data
+        assert par.model_data["gpt-4o"]["max_input_tokens"] == 128000
+
+    def test_get_model_max_input_tokens(self, par: Parser):
+        max_tokens = par.get_model_max_input_tokens()
+        assert max_tokens == 128000
