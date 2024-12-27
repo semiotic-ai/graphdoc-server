ea075afedd9273dd579e051db89dfba93cf16d7a
denver <denver@semiotic.ai>
2024-12-27 15:57:41 -0600

    test: add test_parse_schema against test asset

--- graphdoc/tests/test_parser.py
+++ graphdoc/tests/test_parser.py
@@ -4,10 +4,15 @@ from typing import Dict, Any
 
 # internal packages
 from graphdoc import Prompt, PromptRevision, RequestObject 
+from graphdoc import Parser
 
 # external packages
 import pytest
 
-# class TestParser: 
-#     def test_parser_initialization(self):
+class TestParser: 
+    def test_parse_schema(self, par: Parser): 
+        schema_file = "opensea_original_schema.graphql"
+        schema = par.parse_schema(schema_file)
+        assert schema != None
+        assert len(schema.definitions) == 9
             
