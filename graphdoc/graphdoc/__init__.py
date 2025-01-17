ec06003a97d7901653930f5be8fada6b7ac88de9
denver <denver@semiotic.ai>
2025-01-17 16:16:25 -0600

    feat: add parser class

--- graphdoc/graphdoc/__init__.py
+++ graphdoc/graphdoc/__init__.py
@@ -1,2 +1,4 @@
 from .main import *
 from .evaluate import *
+from .parser import * 
+from .helper import *
---
+++ graphdoc/graphdoc/parser.py
@@ -0,0 +1,19 @@
+# system packages 
+
+# internal packages 
+from typing import Optional
+from .helper import check_directory_path
+
+# external packages 
+
+class Parser: 
+    """
+    A class for the parsing and handling of GraphQL objects. 
+
+    :param schema_directory_path: A path to a directory containing schemas
+    :type schema_directory_path: str
+    """
+
+    def __init__(self, schema_directory_path: Optional[str] = None) -> None:
+        check_directory_path(schema_directory_path)
+        self.schema_directory_path = schema_directory_path
