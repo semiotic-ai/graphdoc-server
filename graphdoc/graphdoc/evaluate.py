ba714eb212a42949efd6d2200839a93a7a69206f
denver <denver@semiotic.ai>
2025-01-17 12:09:29 -0600

    style: black formatting and pyright fixes

--- graphdoc/graphdoc/evaluate.py
+++ graphdoc/graphdoc/evaluate.py
@@ -1,16 +1,17 @@
-# system packages 
+# system packages
 from typing import Literal
 
-# internal packages 
+# internal packages
 
-# external packages 
+# external packages
 import dspy
 from dspy import Signature, InputField, OutputField
 
+
 class DocQuality(Signature):
     """
-    Given a GraphQL Schema, evaluate the quality of documentation for that schema and provide a category rating. 
-    The categories are described as: 
+    Given a GraphQL Schema, evaluate the quality of documentation for that schema and provide a category rating.
+    The categories are described as:
     - Perfect (4): The documentation contains enough information so that the interpretation of the schema and its database content is completely free of ambiguity.
     - Almost Perfect (3): The documentation is almost perfect and free from ambiguity, but there is room for improvement.
     - Somewhat Correct (2): The documentation is somewhat correct but has room for improvement due to missing information. The documentation is not incorrect.
@@ -18,11 +19,11 @@ class DocQuality(Signature):
     Output a number rating that corresponds to the categories described above.
     """
 
-    schema: str = InputField()
+    database_schema: str = InputField()
     category: Literal[
-        "Perfect", 
-        "Almost Perfect", 
-        "Somewhat Correct", 
+        "Perfect",
+        "Almost Perfect",
+        "Somewhat Correct",
         "Incorrect",
     ] = OutputField()
     rating: int = OutputField()
--- graphdoc/graphdoc/main.py
+++ graphdoc/graphdoc/main.py
@@ -1,15 +1,17 @@
 # system packages
 
-# internal packages 
+# internal packages
 
-# external packages 
+# external packages
 import dspy
 
-class GraphDoc: 
-    def __init__(self, 
-                 model: str,
-                 api_key: str,
-                 ) -> None:
-        
+
+class GraphDoc:
+    def __init__(
+        self,
+        model: str,
+        api_key: str,
+    ) -> None:
+
         self.lm = dspy.LM(model=model, api_key=api_key)
         dspy.configure(lm=self.lm)
