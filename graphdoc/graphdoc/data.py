a3d11474a53b5e66293f64d91052d55d76dd1f0d
denver <denver@semiotic.ai>
2025-01-17 16:16:58 -0600

    feat: add DataHelper

---
+++ graphdoc/graphdoc/data.py
@@ -0,0 +1,13 @@
+# system packages 
+
+# internal packages 
+
+# external packages 
+
+class DataHelper: 
+    """
+    A helper class for interacting with external and internal data sets and sources. 
+    """
+    
+    def __init__(self) -> None:
+        pass
