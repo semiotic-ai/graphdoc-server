f4ae3404a095b2e3584470d5f223ddccfde43706
denver <denver@semiotic.ai>
2025-01-17 16:15:20 -0600

    test: add TestConftest.test_par

--- graphdoc/tests/conftest.py
+++ graphdoc/tests/conftest.py
@@ -1,10 +1,12 @@
 # system packages
 import os
 import logging
+from pathlib import Path
 
 # internal packages
 from graphdoc import GraphDoc
 from graphdoc import DocQuality
+from graphdoc import Parser
 
 # external packages
 from pytest import fixture
@@ -18,6 +20,8 @@ log = logging.getLogger(__name__)
 load_dotenv("../.env")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+# Define the base directory (project root)
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 #############################
 # Internal Package Fixtures #
@@ -27,5 +31,10 @@ def gd() -> GraphDoc:
     if OPENAI_API_KEY:
         return GraphDoc(model="openai/gpt-4o-mini", api_key=OPENAI_API_KEY)
     else:
-        log.warn("Missing OPENAI_API_KEY. Ensure .env is properly set.")
+        log.warning("Missing OPENAI_API_KEY. Ensure .env is properly set.")
         return GraphDoc(model="openai/gpt-4o-mini", api_key="filler api key")
+    
+@fixture 
+def par() -> Parser:
+    schema_directory_path = BASE_DIR / "graphdoc" / "tests" / "assets" / "schemas"
+    return Parser(schema_directory_path=str(schema_directory_path))
--- graphdoc/tests/test_conftest.py
+++ graphdoc/tests/test_conftest.py
@@ -4,6 +4,7 @@ import logging
 # internal packages
 from graphdoc import GraphDoc
 from graphdoc import DocQuality
+from graphdoc import Parser
 
 # external packages
 import pytest
@@ -20,3 +21,6 @@ class TestConftest:
         assert isinstance(gd.lm, LM)
         assert isinstance(gd.doc_eval, Predict)
         # assert isinstance(gd.doc_eval.signature, DocQuality) # TODO: this fails...
+
+    def test_par(self, par: Parser):    
+        assert par.schema_directory_path is not None
