d5f88a4b8690ce096bfe5a81931e196038bc458c
denver <denver@semiotic.ai>
2025-01-20 15:52:33 -0600

    test: add test___check_graph_doc_data_dict

--- graphdoc/tests/test_data.py
+++ graphdoc/tests/test_data.py
@@ -10,7 +10,7 @@ from graphdoc import DataHelper
 # external packages
 import pytest
 from dspy import LM, Predict
-from datasets import Features
+from datasets import Features, Dataset
 
 # logging
 logging.basicConfig(level=logging.DEBUG)
@@ -22,3 +22,45 @@ class TestDataHelper:
     def test__get_graph_doc_columns(self, dh: DataHelper):
         features = dh._get_graph_doc_columns()
         assert isinstance(features, Features)
+
+    def test__create_graph_doc_dataset(self, dh: DataHelper):
+        dataset = dh._create_graph_doc_dataset()
+        assert isinstance(dataset, Dataset)
+
+    def test___check_graph_doc_data_dict(self, dh: DataHelper):
+        passing_dict = {
+            "category": ["test"],
+            "rating": ["test"],
+            "schema_name": ["test"],
+            "schema_type": ["test"],
+            "schema_str": ["test"],
+        }
+        missing_key_dict = {
+            "category": ["test"],
+            "rating": ["test"],
+            "schema_name": ["test"],
+            "schema_type": ["test"],
+        }
+        extra_key_dict = {
+            "category": ["test"],
+            "rating": ["test"],
+            "schema_name": ["test"],
+            "schema_type": ["test"],
+            "schema_str": ["test"],
+            "extra_key": ["test"],
+        }
+
+        assert dh._check_graph_doc_data_dict(passing_dict)
+
+        with pytest.raises(ValueError):
+            dh._check_graph_doc_data_dict(missing_key_dict)
+        with pytest.raises(ValueError):
+            dh._check_graph_doc_data_dict(extra_key_dict)
+
+        # wrong_type_dict = {
+        #     "category": [1],
+        #     "rating": ["test"],
+        #     "schema_name": ["test"],
+        #     "schema_type": ["test"],
+        #     "schema_str": "test",
+        # }
