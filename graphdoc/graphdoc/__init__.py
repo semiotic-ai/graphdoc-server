f49ce83641881ea42ea3bfb0ffd13ccaf10ce929
denver <denver@semiotic.ai>
2025-01-20 15:33:15 -0600

    feat: add DataHelper skeleton

--- graphdoc/graphdoc/__init__.py
+++ graphdoc/graphdoc/__init__.py
@@ -2,3 +2,4 @@ from .main import *
 from .evaluate import *
 from .parser import *
 from .helper import *
+from .data import *
--- graphdoc/graphdoc/data.py
+++ graphdoc/graphdoc/data.py
@@ -10,5 +10,41 @@ class DataHelper:
     A helper class for interacting with external and internal data sets and sources.
     """
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+    ) -> None:
         pass
+
+    ######################
+    # loading local data
+    ######################
+
+    # load schemas from a folder, keep the difficulty tag
+
+    # load folder of schemas from a folder, keep the difficulty tag
+
+    # parse out tables from a schema, keep the difficulty tag
+
+    # convert parsed schemas to a dataset
+
+    # convert a folder of schemas to a dataset
+
+    # convert a folder of folders to a dataset
+
+    ######################
+    # hf functions
+    ######################
+
+    # create a dataset
+
+    # pull down a dataset from huggingface
+
+    # check that the dataset has the correct format
+
+    # add new data to a dataset
+
+    # check that there are no duplicates in a dataset
+
+    # deduplicate a dataset
+
+    # upload a dataset to huggingface
