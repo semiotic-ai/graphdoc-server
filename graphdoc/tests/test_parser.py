d0e4804976bde36f48ee91c4d2268d61156d6a03
denver <denver@semiotic.ai>
2025-01-20 13:09:14 -0600

    test: add test_parse_schema_from_file

--- graphdoc/tests/test_parser.py
+++ graphdoc/tests/test_parser.py
@@ -5,10 +5,27 @@ from pathlib import Path
 # internal packages
 
 # external packages
+from graphql import DocumentNode
 import pytest
 
 # logging
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-# class TestParser:
+# Define the base directory (project root)
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+class TestParser:
+
+    def test_parse_schema_from_file(self, par):
+        schema_file = (
+            Path(BASE_DIR)
+            / "graphdoc"
+            / "tests"
+            / "assets"
+            / "schemas"
+            / "opensea_original_schema.graphql"
+        )
+        schema = par.parse_schema_from_file(schema_file)
+        assert isinstance(schema, DocumentNode)
