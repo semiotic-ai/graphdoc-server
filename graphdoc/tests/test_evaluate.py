3e1d62301c12b82cd7373462a244aa77cbb0e03a
denver <denver@semiotic.ai>
2025-01-21 18:51:49 -0600

    test: add test_doc_quality

--- graphdoc/tests/test_evaluate.py
+++ graphdoc/tests/test_evaluate.py
@@ -30,3 +30,11 @@ class TestDocQualityEval:
 
         dqe = DocQualityEval()
         assert dqe.validate_rating(ex, ex)
+
+    @pytest.mark.skipif("not config.getoption('--fire')")
+    def test_doc_quality(self, gd: GraphDoc):
+        dq = DocQuality
+        classify = Predict(dq)
+        classification = classify(database_schema="this is a test, you should reply with a rating of 4 and a category of perfect")
+        assert classification.category == "perfect"
+        assert classification.rating == 4
