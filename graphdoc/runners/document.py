b65ed582fffbcdd22a6320ab33d25db50c6d937e
denver <denver@semiotic.ai>
2025-01-23 01:49:21 -0600

    feat(temp): write documented schemas locally for inspeaction

--- graphdoc/runners/document.py
+++ graphdoc/runners/document.py
@@ -24,26 +24,38 @@ if __name__ == "__main__":
     dh = DataHelper(hf_api_key=HF_DATASET_KEY)
     dge = DocGeneratorEval(dh)
 
-    lm = dspy.LM(model="openai/gpt-4", api_key=OPENAI_API_KEY, cache=False)
+    lm = dspy.LM(model="openai/gpt-4", api_key=OPENAI_API_KEY, cache=True)
     dspy.configure(lm=lm)
 
     dg = dspy.ChainOfThought(DocGenerator)
 
-    dataset = dh._folder_to_dataset(category="perfect", parse_objects=True)
-    trainset = dh._create_graph_doc_example_trainset(dataset=dataset)
+    categories = dh._categories()
+    for category in categories:
+        print(f"Evaluating {category}...")
+        dataset = dh._folder_to_dataset(category=category, parse_objects=True)
+        trainset = dh._create_graph_doc_example_trainset(dataset=dataset)
 
-    evaluator = Evaluate(
-        devset=trainset,
-        num_threads=1,
-        display_progress=True,
-        display_table=5,
-        return_all_scores=True,
+        evaluator = Evaluate(
+            devset=trainset,
+            num_threads=1,
+            display_progress=True,
+            display_table=5,
+            return_all_scores=True,
         return_outputs=True,
-    )
-    overall_score, results, scores = evaluator(dg, dge.evaluate_documentation_quality)
+        )
+        overall_score, results, scores = evaluator(dg, dge.evaluate_documentation_quality)
 
-    for result in results:
-        print("-------------------")
-        print(result[1])
-    print(f"Scores: {scores}")
-    print(f"Overall Score: {overall_score}")
+        c = 0
+        for result in results:
+            print("-------------------")
+            print(result[1])
+            path_category = category.replace(" ", "_")
+            os.makedirs(f"results/{path_category}", exist_ok=True)
+            with open(f"results/{path_category}/result_{c}.graphql", "w") as f:
+                f.write(result[1].documented_schema)
+            c += 1
+        print(f"Scores: {scores}")
+        print(f"Overall Score: {overall_score}")
+        with open(f"results/{path_category}/scores.txt", "w") as f:
+            f.write(f"Scores: {scores}")
+            f.write(f"Overall Score: {overall_score}")
