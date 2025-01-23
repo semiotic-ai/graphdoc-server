9e3e5231ad067727823fc1bf92f0550aa53b081a
denver <denver@semiotic.ai>
2025-01-23 12:25:07 -0600

    feat: add document generator optimzation

--- graphdoc/runners/document.py
+++ graphdoc/runners/document.py
@@ -16,23 +16,65 @@ load_dotenv("../.env")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 HF_DATASET_KEY = os.getenv("HF_DATASET_KEY")
 
+# Run Time Variables
+EVALUATE = False
+OPTIMIZE = True
+MODEL = "openai/gpt-4o"
+CACHE = True
+
 # logging
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    dh = DataHelper(hf_api_key=HF_DATASET_KEY)
-    dge = DocGeneratorEval(dh)
-
-    lm = dspy.LM(model="openai/gpt-4", api_key=OPENAI_API_KEY, cache=True)
+    # setup dspy
+    lm = dspy.LM(model=MODEL, api_key=OPENAI_API_KEY, cache=CACHE)
     dspy.configure(lm=lm)
 
+    # setup graphdoc components
+    dh = DataHelper(hf_api_key=HF_DATASET_KEY)
+    dge = DocGeneratorEval(dh)
     dg = dspy.ChainOfThought(DocGenerator)
 
     categories = dh._categories()
-    for category in categories:
-        print(f"Evaluating {category}...")
-        dataset = dh._folder_to_dataset(category=category, parse_objects=True)
+    
+    if EVALUATE:
+        for category in categories:
+            print(f"Evaluating {category}...")
+            dataset = dh._folder_to_dataset(category=category, parse_objects=True)
+            trainset = dh._create_graph_doc_example_trainset(dataset=dataset)
+
+            evaluator = Evaluate(
+                devset=trainset,
+                num_threads=1,
+                display_progress=True,
+                display_table=5,
+                return_all_scores=True,
+                return_outputs=True,
+            )
+            overall_score, results, scores = evaluator(
+                dg, dge.evaluate_documentation_quality
+            )
+
+            c = 0
+            for result in results:
+                print("-------------------")
+                print(result[1])
+                path_category = category.replace(" ", "_")
+                os.makedirs(f"results/{path_category}", exist_ok=True)
+                with open(f"results/{path_category}/result_{c}.graphql", "w") as f:
+                    f.write(result[1].documented_schema)
+                c += 1
+            print(f"Scores: {scores}")
+            print(f"Overall Score: {overall_score}")
+            with open(f"results/{path_category}/scores.txt", "w") as f:
+                f.write(f"Scores: {scores}")
+                f.write(f"Overall Score: {overall_score}")
+    
+    if OPTIMIZE:
+        log.info("Optimizing Document Generator...")
+        dg.save(f"modules/baseline_document_generator.json", save_program=False)
+        dataset = dh._folder_of_folders_to_dataset(parse_objects=True)
         trainset = dh._create_graph_doc_example_trainset(dataset=dataset)
 
         evaluator = Evaluate(
@@ -43,21 +85,9 @@ if __name__ == "__main__":
             return_all_scores=True,
             return_outputs=True,
         )
-        overall_score, results, scores = evaluator(
-            dg, dge.evaluate_documentation_quality
-        )
 
-        c = 0
-        for result in results:
-            print("-------------------")
-            print(result[1])
-            path_category = category.replace(" ", "_")
-            os.makedirs(f"results/{path_category}", exist_ok=True)
-            with open(f"results/{path_category}/result_{c}.graphql", "w") as f:
-                f.write(result[1].documented_schema)
-            c += 1
-        print(f"Scores: {scores}")
-        print(f"Overall Score: {overall_score}")
-        with open(f"results/{path_category}/scores.txt", "w") as f:
-            f.write(f"Scores: {scores}")
-            f.write(f"Overall Score: {overall_score}")
+        tp = dspy.MIPROv2(metric=dge.evaluate_documentation_quality, auto="light")
+        optimized_generator = tp.compile(
+            dg, trainset=trainset, max_labeled_demos=0, max_bootstrapped_demos=0
+        )
+        optimized_generator.save(f"modules/optimized_document_generator.json", save_program=False)
