dbc6825e604b949dacd24f5c643788042c150ee2
denver <denver@semiotic.ai>
2025-01-22 22:59:14 -0600

    chore: add logging to evaluate_documentation_quality

--- graphdoc/graphdoc/generate.py
+++ graphdoc/graphdoc/generate.py
@@ -94,13 +94,15 @@ class DocGeneratorEval:
             gold_schema = parse(schema.database_schema)
             pred_schema = parse(pred.documented_schema)
         except Exception as e:
-            log.warning(f"An exception occurred while parsing the schema: {e}")
+            log.warning(f"evaluate_documentation_quality: An exception occurred while parsing the schema: {e}")
             return 1 
             # raise ValueError(f"An exception occurred while parsing the schema: {e}")
         if not self.dh.par.schema_equality_check(gold_schema, pred_schema):
+            log.warning("evaluate_documentation_quality: Schema equality check failed")
             return 1
 
         evaluation = dspy.Predict(DocQuality)(database_schema=pred.documented_schema)
+        log.info(f"evaluate_documentation_quality: Evaluation: {evaluation.rating}")
         return evaluation.rating
 
 
