4ce0a672f11cf471a2ebeae981f4be60ee0a0eab
denver <denver@semiotic.ai>
2025-01-22 20:16:44 -0600

    feat: add DocGeneratorEval functionality

--- graphdoc/graphdoc/generate.py
+++ graphdoc/graphdoc/generate.py
@@ -2,17 +2,89 @@
 import logging
 from typing import Callable, List, Literal, Optional, Union
 
+from graphdoc.evaluate import DocQuality
+from graphql import parse
+
 # internal packages
 from .data import DataHelper
 
+
 # external packages
 import dspy
-from dspy import Example
+from dspy import Example, Prediction
 from dspy.evaluate import Evaluate
 
 # logging
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
+
+
+class DocGeneratorEval:
+    """
+    A helper class for dealing with evaluation of DocGenerator.
+    """
+
+    def __init__(self, dh: Optional[DataHelper] = None) -> None:
+        if dh:
+            self.dh = dh
+        else:
+            self.dh = DataHelper()
+
+    def validate_schema_format(
+        self, entity: Example, prediction: Prediction, trace=None
+    ) -> bool:
+        """
+        A helper function to validate the schema format of the prediction.
+
+        :param entity: The entity to validate against
+        :type entity: Example
+        :param prediction: The prediction to validate
+        :type prediction: Prediction
+        :param trace: The trace of the prediction
+        :type trace: Optional[str]
+        :return: Whether the prediction is correct
+        :rtype: bool
+        """
+        try:
+            input_schema = entity.database_schema
+            output_schema = prediction.documented_schema
+
+            if isinstance(input_schema, str) and isinstance(output_schema, str):
+                input_ast = parse(input_schema)
+                output_ast = parse(output_schema)
+                return self.dh.par.schema_equality_check(input_ast, output_ast)
+            else:
+                return False
+        except Exception as e:
+            log.warning(f"Schema format validation failed due to error: {e}")
+            return False
+
+    def preprocess_schema(self, entity: Example) -> Example:
+        """
+        A helper function to preprocess the schema.
+
+        :param entity: The entity to preprocess
+        :type entity: Example
+        :return: The preprocessed entity
+        :rtype: Example
+        """
+
+        return entity
+
+    def evaluate_documentation_quality(self, pred: Prediction, trace=None) -> int:
+        """
+        A helper function to evaluate the quality of the documentation.
+
+        :param pred: The prediction to evaluate
+        :type pred: Prediction
+        :param trace: The trace of the prediction
+        :type trace: Optional[str]
+        :return: The quality of the documentation
+        :rtype: int
+        """
+        evaluation = dspy.Predict(DocQuality)(database_schema=pred.documented_schema)
+        return evaluation.rating
+
 
 #################
 # DSPy Modules  #
@@ -28,8 +100,11 @@ class DocGenerator(dspy.Signature):
     - Exclude any speculative or additional commentary.
     - DO NOT return the phrase "in the { table } table" in your description.
 
-    ### Formatting 
-    - Ensure that the schema maintains proper documentation formatting, as is provided. 
+    ### Formatting
+    - Ensure that the schema maintains proper documentation formatting, as is provided.
     """
+
     database_schema: str = dspy.InputField()
-    documented_schema: str = dspy.OutputField()
+    documented_schema: str = dspy.OutputField(
+        desc="The database schema with proper documentation, ensuring that the underlying schema is not altered."
+    )
