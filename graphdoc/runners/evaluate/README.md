966fbcc4f7c445b842d3cdff1b88cb3722ea8177
denver <denver@semiotic.ai>
2025-01-24 13:46:57 -0600

    docs(example): add mlflow+dspy model saving example

---
+++ graphdoc/runners/evaluate/README.md
@@ -0,0 +1,16 @@
+# Evaluate
+
+This is primarily an PoC of using DSPy and MLflow together. We will go through the basic steps of using MLFlow for monitoring our training and evaluation of a DSPy model. We will go through the basic steps of using MLFlow to save and load a DSPy model. We will go through the basic steps of using MLFlow to trace a DSPy model. From there, we will have the foundation to implement this in a more modular fashion within the graphdoc project.
+
+## Basic Steps
+
+1. Managing the DSPy / MLFlow relationship
+    - [ ] Create a DSPy model
+    - [ ] Create a MLFlow model that tracks to that DSPy model 
+    - [ ] Save the DSPy model to MLFlow
+2. Training / Evaluation pipeline 
+    - [ ] load in a given MLFlow model
+    - [ ] assess if it was trained on our most recent dataset
+    - [ ] if not trained, train it on our most recent dataset
+    - [ ] assess performance of the trained model on our most recent dataset
+    - [ ] save the trained model to MLFlow
---
+++ graphdoc/runners/evaluate/model_saving.py
@@ -0,0 +1,80 @@
+# system packages
+import os
+import logging
+
+# internal packages
+from graphdoc import GraphDoc, DataHelper, DocQualityEval, DocQuality
+
+# external packages
+import dspy
+import mlflow
+from mlflow.models import infer_signature
+from dotenv import load_dotenv
+
+# Environment Variables
+load_dotenv("../.env")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+HF_DATASET_KEY = os.getenv("HF_DATASET_KEY")
+
+# Run Time Variables
+EXPERIMENT_NAME = "eval_model_saving"
+DSPY_MODEL_NAME = "DocQuality_zero_shot"
+MODEL = "openai/gpt-4o-mini"
+CACHE = True
+FIRST_RUN = True
+
+# logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+# MLFlow 
+mlflow.dspy.autolog()
+mlflow.set_experiment(EXPERIMENT_NAME)
+
+# set up DSPy and GraphDoc components
+dh = DataHelper(hf_api_key=HF_DATASET_KEY)
+dqe = DocQualityEval(dh)
+
+lm = dspy.LM(model=MODEL, api_key=OPENAI_API_KEY, cache=CACHE)
+dspy.configure(lm=lm)
+classify = dspy.Predict(DocQuality)
+
+# load in the dataset
+dataset = dh._folder_to_dataset(category="perfect", parse_objects=True)
+trainset = dh._create_graph_doc_example_trainset(dataset=dataset)
+
+example = trainset[0].toDict()
+# drop the keys 'category' and 'rating'
+example.pop('category')
+example.pop('rating')
+print(example)
+
+signature = infer_signature(example)
+print(signature)
+
+with mlflow.start_run():
+    
+    if FIRST_RUN:
+        
+        model_info = mlflow.dspy.log_model(
+            dspy_model=classify, 
+            artifact_path="model",
+            signature=signature,
+            task=None,
+            registered_model_name=DSPY_MODEL_NAME,
+            metadata={"trainset": "semiotic/graphdoc_schemas"} # establish versioning for trainset
+        )
+        print(f"The model was logged to MLFlow as {model_info.model_uri}")
+
+        loaded_model = mlflow.dspy.load_model(model_info.model_uri)
+        pred = loaded_model(database_schema=trainset[1].database_schema).rating
+        print(f"The model predicted a rating of {pred}")
+
+# 1. Managing the DSPy / MLFlow relationship
+#     - [x] Create a DSPy model
+#     - [x] Create a MLFlow model that tracks to that DSPy model 
+#     - [x] Save the DSPy model to MLFlow
+
+# Notes 
+# - we are running with a local environment setting, and will want to migrate to using a 
+# - even with no changes, MLFlow will log the model as a new version
