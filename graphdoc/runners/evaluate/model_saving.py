d001ff1b2c259bad74ac8eab6ebef88b9b982c0c
denver <denver@semiotic.ai>
2025-01-26 18:42:35 -0600

    docs: add model name based retrieval

--- graphdoc/runners/evaluate/model_saving.py
+++ graphdoc/runners/evaluate/model_saving.py
@@ -21,7 +21,7 @@ EXPERIMENT_NAME = "eval_model_saving"
 DSPY_MODEL_NAME = "DocQuality_zero_shot"
 MODEL = "openai/gpt-4o-mini"
 CACHE = True
-FIRST_RUN = True
+FIRST_RUN = False
 
 # logging
 logging.basicConfig(level=logging.DEBUG)
@@ -30,6 +30,7 @@ log = logging.getLogger(__name__)
 # MLFlow 
 mlflow.dspy.autolog()
 mlflow.set_experiment(EXPERIMENT_NAME)
+ml_client = mlflow.MlflowClient()
 
 # set up DSPy and GraphDoc components
 dh = DataHelper(hf_api_key=HF_DATASET_KEY)
@@ -43,19 +44,21 @@ classify = dspy.Predict(DocQuality)
 dataset = dh._folder_to_dataset(category="perfect", parse_objects=True)
 trainset = dh._create_graph_doc_example_trainset(dataset=dataset)
 
+# get an example to use to infer the signature
 example = trainset[0].toDict()
 # drop the keys 'category' and 'rating'
 example.pop('category')
 example.pop('rating')
 print(example)
 
+# infer the signature based on the example
 signature = infer_signature(example)
 print(signature)
 
 with mlflow.start_run():
     
     if FIRST_RUN:
-        
+        # log the model to MLFlow
         model_info = mlflow.dspy.log_model(
             dspy_model=classify, 
             artifact_path="model",
@@ -67,6 +70,16 @@ with mlflow.start_run():
         print(f"The model was logged to MLFlow as {model_info.model_uri}")
 
         loaded_model = mlflow.dspy.load_model(model_info.model_uri)
+        pred = loaded_model(database_schema=trainset[1].database_schema).rating
+        print(f"The model predicted a rating of {pred}")
+    
+    else: 
+        # get the latest version of the model
+        latest_version = ml_client.get_latest_versions(DSPY_MODEL_NAME)
+        print(f"The latest version of the model is {latest_version}")
+
+        # load the latest model 
+        loaded_model = mlflow.dspy.load_model(latest_version[0].source)
         pred = loaded_model(database_schema=trainset[1].database_schema).rating
         print(f"The model predicted a rating of {pred}")
 
