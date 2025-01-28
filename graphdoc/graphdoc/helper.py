4e022011aa2f4c907969a72b0e110d96dc9e968c
denver <denver@semiotic.ai>
2025-01-28 13:49:04 -0600

    feat: add load_yaml_config

--- graphdoc/graphdoc/helper.py
+++ graphdoc/graphdoc/helper.py
@@ -1,5 +1,7 @@
 # system packages
+import os
 import yaml
+from yaml import SafeLoader
 from pathlib import Path
 from typing import Union
 
@@ -22,7 +24,24 @@ def check_file_path(file_path: Union[str, Path]) -> None:
         raise ValueError(
             f"The provided path does not resolve to a valid file: {file_path}"
         )
-    
-def load_yaml_config(file_path: Union[str, Path]) -> dict:
-    with open(file_path, "r") as file:
-        return yaml.safe_load(file)
+
+
+def _env_constructor(loader, node):
+    value = loader.construct_scalar(node)
+    env_value = os.getenv(value)
+    if env_value is None:
+        raise ValueError(f"Environment variable '{value}' is not set.")
+    return env_value
+
+
+def load_yaml_config(file_path: Union[str, Path], use_env: bool = True) -> dict:
+    if use_env:
+        SafeLoader.add_constructor("!env", _env_constructor)
+
+    _file_path = Path(file_path).resolve()
+    if not _file_path.is_file():
+        raise ValueError(
+            f"The provided path does not resolve to a valid file: {file_path}"
+        )
+    with open(_file_path, "r") as file:
+        return yaml.load(file, Loader=SafeLoader)
