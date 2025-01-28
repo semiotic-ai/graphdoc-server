86e14c41aaf3fd1440ea832b88f300b8194c549f
denver <denver@semiotic.ai>
2025-01-27 18:04:02 -0600

    test: add DocQualityPrompt.evaluate_metric

--- graphdoc/tests/test_schema_doc_quality.py
+++ graphdoc/tests/test_schema_doc_quality.py
@@ -3,6 +3,7 @@ import logging
 
 # internal packages
 from graphdoc.prompts import DocQualityPrompt, DocQualitySignature, SinglePrompt
+from graphdoc import DataHelper, GraphDoc
 
 # external packages
 import dspy
@@ -32,3 +33,25 @@ class TestSchemaDocQuality:
         dqp = DocQualityPrompt(type="chain_of_thought", metric_type="rating")
         p = dqp.get_chain_of_thought()
         assert isinstance(p, dspy.ChainOfThought)
+
+    def test_evaluate_metric(self, gd: GraphDoc):
+        example = dspy.Example(
+            database_schema="this is a test, you should reply with a rating of 4 and a category of perfect",
+            category="perfect",
+            rating=4,
+        )
+        prediction = dspy.Prediction(
+            category="perfect",
+            rating=4,
+        )
+        failing_prediction = dspy.Prediction(
+            category="fail",
+            rating=3,
+        )
+        dqp = DocQualityPrompt(type="predict", metric_type="rating")
+        assert dqp.evaluate_metric(example, prediction)
+        assert not dqp.evaluate_metric(example, failing_prediction)
+
+        dqp = DocQualityPrompt(type="predict", metric_type="category")
+        assert dqp.evaluate_metric(example, prediction)
+        assert not dqp.evaluate_metric(example, failing_prediction)
