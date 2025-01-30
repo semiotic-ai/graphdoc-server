68166d7e6881e86160a5dfb92f580aa879f231ee
denver <denver@semiotic.ai>
2025-01-30 12:57:31 -0600

    test: add test_doc_generator_prompt_init_from_config

--- graphdoc/tests/test_doc_generator_prompt.py
+++ graphdoc/tests/test_doc_generator_prompt.py
@@ -1,5 +1,6 @@
 # system packages
 import logging
+from pathlib import Path
 
 # internal packages
 from graphdoc.prompts import DocGeneratorPrompt, DocQualityPrompt, SinglePrompt
@@ -12,6 +13,8 @@ import dspy
 # logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
+# global variables
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 class TestDocGeneratorPrompt:
     def test_doc_generator_prompt(self):
@@ -21,4 +24,16 @@ class TestDocGeneratorPrompt:
         dqp = DocQualityPrompt(type="predict", metric_type="rating")
         dgp = DocGeneratorPrompt(type="chain_of_thought", metric_type=dqp)
         assert isinstance(dgp, SinglePrompt)
+        assert isinstance(dgp.infer, dspy.ChainOfThought)
+    
+    def test_doc_generator_prompt_init_from_config(self, gd: GraphDoc):
+        config_path = BASE_DIR / "graphdoc" / "tests" / "assets" / "configs" / "single_prompt_schema_doc_generator_trainer.yaml"
+        metric_config_path = BASE_DIR / "graphdoc" / "tests" / "assets" / "configs" / "single_prompt_schema_doc_quality_trainer.yaml"
+
+        dgp = gd._get_nested_single_prompt(
+            config_path=config_path,
+            metric_config_path=metric_config_path,
+        )
+        assert isinstance(dgp, SinglePrompt)
+        assert isinstance(dgp, DocGeneratorPrompt)
         assert isinstance(dgp.infer, dspy.ChainOfThought)
