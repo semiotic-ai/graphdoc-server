12d0f4cb7846f75fa6df70658b99440ca15d19c8
denver <denver@semiotic.ai>
2025-03-17 13:46:29 -0500

    refactor: utilize logging_id in document_full_schema

--- graphdoc-server/graphdoc_server/app.py
+++ graphdoc-server/graphdoc_server/app.py
@@ -225,7 +225,7 @@ def create_app() -> Flask:
                     trace=True,
                     client=mdh.mlflow_client,  # type: ignore # we explicitely check for graphdoc.mdh is not None
                     expirement_name=config_contents["server"]["mlflow_experiment_name"],
-                    api_key=request.headers[
+                    logging_id=request.headers[
                         "X-API-Key"
                     ],  # record the api key that made the request
                 )
